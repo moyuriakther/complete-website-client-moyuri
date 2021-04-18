ee85c2622c5f1516a07789ca929c593a8d2f4203
Moyuri Akther <akthermoyuri@.com>
2021-04-19 02:39:08 +0600

    up

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const bodyParser = require("body-Parser");
 require('dotenv').config()
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5050
 
 app.use(cors());
 app.use(bodyParser.json());
